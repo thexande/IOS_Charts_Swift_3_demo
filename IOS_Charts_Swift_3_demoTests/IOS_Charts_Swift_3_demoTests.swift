991a8db2dc3eb8d5c7e8b3c2d0070c02370cc491
Alexander Murphy <amurphy345@gmail.com>
2016-09-24 15:30:43 -0600

    Initial Commit

---
+++ IOS_Charts_Swift_3_demoTests/IOS_Charts_Swift_3_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  IOS_Charts_Swift_3_demoTests.swift
+//  IOS_Charts_Swift_3_demoTests
+//
+//  Created by Alexander Murphy on 9/24/16.
+//  Copyright © 2016 Alexander Murphy. All rights reserved.
+//
+
+import XCTest
+@testable import IOS_Charts_Swift_3_demo
+
+class IOS_Charts_Swift_3_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
